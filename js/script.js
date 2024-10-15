e575027c93d489f587b6a2c80becde8b8caec0cd
antoniorusso2 <russoantonio303@outlook.it>
2024-10-15 14:56:38 +0200

    ADD: index.html + js folder with pseudocode

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+
+//STAMPARE I NUMERI DA 1 A 100
+  //inizializzazione variabile 'num' senza valori all'interno;
+  //'for' loop che si ripete 100 volte partendo da 0 ed aggiungendo 1 ad ogni ciclo
+    //nel blocco di codice inserire il valore derlla precedente variabile 'num' dichiarata all'esterno ed aggiungere 1 [num = i + 1]
+
+  //PER I MULTIPLI DI 3 STAMPARE Fizz
+  // IF = verifica multiplo di 3
+    //utilizzo dell'operatore % [remainder] in base al numero dato dal loop; [remainderThree = num % 3 => valori number da 0 a 2] //quindi se remainderThree = 0 stampare 'Fizz'
+
+  //PER I MULTIPLI DI 5 STAMPARE Buzz
+  // ELSE = verifica multiplo di 5
+    //utilizzo dell'operatore % [remainder] in base al numero dato dal loop; [remainderFive = num % 5 => valori number da 0 a 4] //quindi se remainderFive = 0 stampare 'Buzz'
+  // IF = remaiderThree AND [&&] remainderFive = 0 => stampare 'FizzBuzz';
+
+  //OUTPUT
+
