6e89a13416e6b2da3db74ff30b5376c51cf3b609
antoniorusso2 <russoantonio303@outlook.it>
2024-10-15 15:35:51 +0200

    ADD: count from 1 to 100 loop with Fizz replacing all multiples of 3 and Buzz replacing all multiples of 5

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
-
-
-
 //STAMPARE I NUMERI DA 1 A 100
-  //inizializzazione variabile 'num' senza valori all'interno;
+  //inizializzazione variabile 'num' senza valori all'esterno;
   //'for' loop che si ripete 100 volte partendo da 0 ed aggiungendo 1 ad ogni ciclo
-    //nel blocco di codice inserire il valore derlla precedente variabile 'num' dichiarata all'esterno ed aggiungere 1 [num = i + 1]
+  let num ;
+  for (i = 0; i < 100; i++) {
+    //nel blocco di codice inserire il valore della precedente variabile 'num' dichiarata all'esterno ed aggiungere 1 [num = i + 1]
+     num = i + 1;
+    // console.log(num)//numbers da 1 a 100
 
-  //PER I MULTIPLI DI 3 STAMPARE Fizz
-  // IF = verifica multiplo di 3
+    //PER I MULTIPLI DI 3 STAMPARE Fizz
+    //inizializzazione varibile 
     //utilizzo dell'operatore % [remainder] in base al numero dato dal loop; [remainderThree = num % 3 => valori number da 0 a 2] //quindi se remainderThree = 0 stampare 'Fizz'
-
-  //PER I MULTIPLI DI 5 STAMPARE Buzz
-  // ELSE = verifica multiplo di 5
+    let remainderThree = num % 3;
+    
+    //PER I MULTIPLI DI 5 STAMPARE Buzz
+    // ELSE = verifica multiplo di 5
     //utilizzo dell'operatore % [remainder] in base al numero dato dal loop; [remainderFive = num % 5 => valori number da 0 a 4] //quindi se remainderFive = 0 stampare 'Buzz'
-  // IF = remaiderThree AND [&&] remainderFive = 0 => stampare 'FizzBuzz';
+    let remainderFive = num % 5;
 
-  //OUTPUT
+    // IF = verifica multiplo di 3
+    if (remainderThree === 0) {
+      num = 'Fizz' // string
+      
+    }
+    else if (remainderFive === 0){
+      num = 'Buzz' //string
+    }
+
+    // IF = remaiderThree AND [&&] remainderFive = 0 => stampare 'FizzBuzz';
+    if (remainderFive === 0 && remainderThree === 0){
+      num = 'FizzBuzz' // string
+    }
+    console.log(num) // multipli di 3 e 5 sostituiti con Fizzbuzz
+  }
+    
 
